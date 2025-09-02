77f3f2b6f80180e4e7f02ef326c7177e52000a2d
Andreas Rosenthal <arosenthal@ssystems.de>
2025-09-02 16:09:20 +0200

    working confetti

--- amd/src/confetti.js
+++ amd/src/confetti.js
@@ -5,6 +5,11 @@
  * @copyright  2025 YOUR NAME <your@email.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+import confettilib from './confettilib';
+
 export const init = () => {
         window.console.log('Confetti init');
+        confettilib();
+        window.console.log('Confetti done');
 };
