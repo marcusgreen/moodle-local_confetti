08b98dcb3a72e1c039fc32fa15ce6dd46455ed69
Odei Alba <odeialba@gmail.com>
2025-09-02 16:39:18 +0200

    Make confetti work with grunt

--- amd/src/confetti.js
+++ amd/src/confetti.js
@@ -6,10 +6,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-import confettilib from './confettilib';
+import confetti from './confetti/confetti';
 
 export const init = () => {
         window.console.log('Confetti init');
-        confettilib();
+        confetti();
         window.console.log('Confetti done');
 };
